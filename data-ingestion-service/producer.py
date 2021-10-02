0ebe63382f4258972018854d1d0acdb8380a03d8
Vedha286 <vedhakrishna.v@gmail.com>
2021-10-02 16:31:00 +0200

    can get data from the free-news api

--- data-ingestion-service/producer.py
+++ data-ingestion-service/producer.py
@@ -1,19 +1,38 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 import json 
+import requests
+
+free_news_url = "https://free-news.p.rapidapi.com/v1/search"
+free_news_headers = {
+    'x-rapidapi-host': "free-news.p.rapidapi.com",
+    'x-rapidapi-key': "a074ca3a54mshd2b31882937309fp11ba8bjsndbc6662232d3"
+}
+news_train_topic = "news-train"
+
+producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'],
+	value_serializer=lambda v: json.dumps(v).encode('utf-32'))
+
+
+def get_free_news_response(query, url, headers):
+	querystring = {"q":query,"lang":"en"}
+
+	response = json.loads(requests.request("GET", url, headers=headers, params=querystring).text)
+	
+	return response["articles"]
 
 def error_callback(exc):
     raise Exception('Error while sendig data to kafka: {0}'.format(str(exc)))
 
 def write_to_kafka(topic_name, items):
-
-  	print(items)
+  	#print(items)
 	count = 0
 	for item in items:
 		producer.send(topic_name ,value=item).add_errback(error_callback)
 		count = count + 1
   	producer.flush()
 	print("Wrote {0} messages into topic: {1}".format(count, topic_name))
+
 
 
 mockNews = [{
@@ -24,7 +43,7 @@ mockNews = [{
 	"description":"something..."
 	}]
 
-producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'],
-	value_serializer=lambda v: json.dumps(v).encode('utf-32'))
-write_to_kafka("news-train", mockNews)
+write_to_kafka(news_train_topic, get_free_news_response("Elon Musk", free_news_url, free_news_headers))
+write_to_kafka(news_train_topic, get_free_news_response("Trump", free_news_url, free_news_headers))
+write_to_kafka(news_train_topic, get_free_news_response("India", free_news_url, free_news_headers))
 
