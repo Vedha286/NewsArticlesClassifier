3151833cdb974cd98a74938c5e1f9ed8d531e171
Vedha286 <vedhakrishna.v@gmail.com>
2021-10-02 21:30:50 +0200

    can connet to db

--- data-ingestion-service/consumer.py
+++ data-ingestion-service/consumer.py
@@ -1,11 +1,28 @@
 from kafka import KafkaConsumer
 import json 
+from pymongo import MongoClient
 
-consumer = KafkaConsumer(bootstrap_servers=['127.0.0.1:9092'],
-	value_deserializer=lambda m: json.loads(m.decode('utf-32')))
+client = MongoClient("mongodb+srv://IIITH-group10:LeoEXtI5sxntXmpG@cluster0.jejzt.mongodb.net/news?retryWrites=true&w=majority")
 
-consumer.subscribe(['news-train'])
+db = client.news
 
-for message in consumer:
-	newAtricle = message.value
-	print (newAtricle)
+try: 
+	print("You are connected")
+	news_collection =db["newsCollections"]
+	db.newsCollections.insert_one({"test":"test1"})
+	
+	client.close() 
+except Exception as e: print(e)
+
+
+#db = client.news
+
+
+#consumer = KafkaConsumer(bootstrap_servers=['127.0.0.1:9092'],
+#	value_deserializer=lambda m: json.loads(m.decode('utf-32')))
+
+#consumer.subscribe(['news-train'])
+
+#for message in consumer:
+#	newAtricle = message.value
+#	print (newAtricle)
