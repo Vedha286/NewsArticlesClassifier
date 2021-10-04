da77d18d27b8c2320a44bd752029b4981ff54bbc
Vedha286 <vedhakrishna.v@gmail.com>
2021-10-04 22:10:47 +0200

    refactored code to get data periodically

--- data-ingestion-service/kafka-queue/consumer.py
+++ data-ingestion-service/kafka-queue/consumer.py
@@ -6,23 +6,32 @@ from pymongo import MongoClient
 client = MongoClient("mongodb+srv://IIITH-group10:LeoEXtI5sxntXmpG@cluster0.jejzt.mongodb.net/news?retryWrites=true&w=majority")
 
 db = client.news
-news_collection =db["newsCollections"]
+news_collection =db["newsArticles"]
+db_errors = []
 
-def save_all_documents_to_db(doc):
+def save_all_documents_to_db(docs):
 
-	try: 
-		print("Saving " + str(len(doc)) + " documents")
-		db.newsCollections.insert_many(doc)
-		print("Saved " + str(len(doc)) + " documents")
+	print("Saving " + str(len(docs)) + " documents")
+	pass_count = 0
+	fail_count = 0
+	for doc in docs:
+		try: 
+			db.newsArticles.insert_one(doc)
+
+			pass_count = pass_count + 1
 	
-	except Exception as e: 
-		print(e)
+		except Exception as e: 
+			db_errors.append(e)
+			fail_count = fail_count + 1
+	print("Saved " + str(pass_count) + " documents")
+	print(str(fail_count) + " documents failed to save")
 
 consumer = KafkaConsumer(bootstrap_servers=['127.0.0.1:9092'], group_id='news_group')
 
 def processResult(status, message, job, result, exception, stacktrace):
 	if status == 'success':
 		print('Results length: ' + str(len(result)))
+		print(result)
 		
 		if(len(result)>0):
 			save_all_documents_to_db(result)
--- data-ingestion-service/kafka-queue/producer.py
+++ data-ingestion-service/kafka-queue/producer.py
@@ -14,73 +14,131 @@ free_news_headers = {
     'x-rapidapi-host': "free-news.p.rapidapi.com",
     'x-rapidapi-key': "a074ca3a54mshd2b31882937309fp11ba8bjsndbc6662232d3"
 }
+free_news_daily_limit = 100
 
-free_news_query_keywords = ["India", "Elon musk", "space travel", "floods"]
-free_news_query_keywords = ["India"]
+newscather_url = "https://api.newscatcherapi.com/v2/search"
+newscather_headers = {
+    'x-api-key': "5rFJMD3k9h-EPR02GDjEb_3M7eJeJNpEg5Hv7rW15T0"
+}
 
-news_api = NewsApiClient(api_key='236cd597a4e2440abe9da95ecf2e1f79')
+newscather_daily_limit = 1000
+times_per_day = 6
 
 
-def format_free_news_time(t, datetime):
+query_keywords = [{"keyword":"India", "isComplete":False}, 
+{"keyword":"Elon musk", "isComplete":False}, 
+{"keyword":"space travel", "isComplete":False}, 
+{"keyword":"Vedha", "isComplete":False}]
+
+def format_article_date_time(t, datetime):
     return datetime.strptime(t, "%Y-%m-%d %H:%M:%S")
 
-
-def get_free_news_response(query, url, headers, r, datetime, format_time):
-    querystring = {"q": query, "lang": "en", "page_size": 2}
+def get_free_news_response(query, url, headers, r, datetime, format_time,time,limit):
+    
+    articles = []	
+    page_count = 1
+    stop = False
+    page_size = 25
+    querystring = {"q": query, "lang": "en", "page_size": page_size, "page": page_count}
 
     response = r.get(url, headers=headers, params=querystring).json()
 
-    if("status" in response):
-        if(response["status"] == 'ok'):
-            articles = []
-            for article in response["articles"]:
-                article_resopnse = {"title": article['title'], "date": format_time(
-                    article["published_date"], datetime), "summary": article["summary"], "category": article["topic"], "source": article["link"]}
-                articles.append(article_resopnse)
-            response = articles
-    else:
-        response = []
-    return response
+    while not stop:
+        if("status" in response):
+            if(response["status"] == 'ok'):
+                print("page_count : " + str(page_count) + "   total_pages: " + str(response["total_pages"]))
 
-def format_news_api_time(t, datetime):
-    return datetime.strptime(t, "%Y-%m-%dT%H:%M:%SZ")
+                page_count = page_count + 1
+                
+                for article in response["articles"]:
+                    article_resopnse = {"title": article['title'], "date": format_time(article["published_date"], datetime), "summary": article["summary"], "category": article["topic"], "source": article["link"]}
+                    articles.append(article_resopnse)
+                
+                if(page_count == response["total_pages"] or page_count >= limit):
+                    stop = True
+
+                else:
+                    time.sleep(1)
+                    querystring = {"q": query, "lang": "en", "page_size": page_size, "page": page_count}
+			
+                    response = r.get(url, headers=headers, params=querystring).json()
+            elif(response["status"] != 'ok'):
+                print(response)
+                stop = True
+        elif("status" not in response):
+            print(response)
+            stop = True
+    print("=====================================================\n\n\n")
+
+    return articles
 
 
-def get_news_api_response(query, news_api, datetime, format_time):
-	
-    response = news_api.get_everything(q=query,
-                                      language='en',
-                                      sort_by='popularity',
-				      page_size=2,
-                                      page=1)
+def get_newscather_response(query, url, headers, r, datetime, format_time,time,limit):
+    
+    articles = []	
+    page_count = 1
+    stop = False
+    page_size = 100
+    querystring = {"q": query, "lang": "en", "page_size": page_size, "sort_by":"date", "page": page_count}
 
-    if("status" in response):
-        if(response["status"] == 'ok'):
-            articles = []
-            for article in response["articles"]:
-                article_resopnse = {"title": article['title'], "date": format_time(
-                    article["publishedAt"], datetime), "summary": article["content"], "category": None, "source": article["url"]}
-                articles.append(article_resopnse)
-            response = articles
-    else:
-        response = []
-    return response
+    response = r.get(url, headers=headers, params=querystring).json()
+
+    while not stop:
+        if("status" in response):
+            if(response["status"] == 'ok'):
+                print("page_count : " + str(page_count) + "   total_pages: " + str(response["total_pages"]))
+
+                page_count = page_count + 1
+                
+                for article in response["articles"]:
+                    article_resopnse = {"title": article['title'], "date": format_time(article["published_date"], datetime), "summary": article["summary"], "category": article["topic"], "source": article["link"]}
+                    articles.append(article_resopnse)
+                
+                if(page_count == response["total_pages"] or page_count >= limit):
+                    stop = True
+
+                else:
+                    time.sleep(1)
+                    querystring = {"q": query, "lang": "en", "page_size": page_size, "sort_by":"date", "page": page_count}
+			
+                    response = r.get(url, headers=headers, params=querystring).json()
+            elif(response["status"] != 'ok'):
+                print(response)
+                stop = True
+        elif("status" not in response):
+            stop = True
+            print(response)
+    print("=====================================================\n\n\n")
+
+    return articles
+
 
 producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092')
 
 queue = Queue(topic=news_train_topic, producer=producer)
-
+times_per_day_to_run = 0
 # scheduler code starts
 def get_data_from_apis():
-    for query in free_news_query_keywords:
-        queue.enqueue(get_free_news_response, query, free_news_url,
-                      free_news_headers, requests, datetime, format_free_news_time)
-        queue.enqueue(get_news_api_response, query, news_api, datetime, format_news_api_time)
-    producer.flush()
+	global times_per_day_to_run
+	for query in query_keywords:
+		print(query["keyword"])
+		if(not query["isComplete"]):
+			
+			queue.enqueue(get_free_news_response, query["keyword"], free_news_url,
+                      free_news_headers, requests, datetime, format_article_date_time,time, free_news_daily_limit/times_per_day)
 
+			queue.enqueue(get_newscather_response, query["keyword"], newscather_url, newscather_headers, requests, datetime, format_article_date_time, time, newscather_daily_limit/times_per_day)
+			query["isComplete"] = True
+	producer.flush()
+	times_per_day_to_run = times_per_day_to_run + 1	
+	if(times_per_day_to_run == 24/times_per_day):
+		print("All the keywords ran " + str(times_per_day_to_run))
+		times_per_day_to_run = 0
+		for query in query_keywords:
+			query["isComplete"] = False
 
 # After an interval calling the below functions
-schedule.every(20).minutes.do(get_data_from_apis)
+schedule.every(10).seconds.do(get_data_from_apis)
 
 while True:
     schedule.run_pending()
