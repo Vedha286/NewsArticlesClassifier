29548b2d465bcb29ab2cda9eb1af7603721cffc0
Vedha286 <vedhakrishna.v@gmail.com>
2021-10-15 17:42:28 +0200

    error

--- model-training-service/data_cleaning.py
+++ model-training-service/data_cleaning.py
@@ -1,6 +1,7 @@
 from pyspark import SparkContext                                                                                        
 from pyspark.sql import SparkSession                                                                                    
 from pyspark.streaming import StreamingContext   
+from pyspark.sql import SQLContext
 # import keys as conf
 
 '''
@@ -21,12 +22,15 @@ mongodb_connection_string = "mongodb+srv://IIITH-group10:LeoEXtI5sxntXmpG@cluste
 
 spark = SparkSession.\
 builder.\
-appName("pyspark-notebook2").\
+appName("newsClassifier").\
+config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0").\
 config("spark.mongodb.input.uri", mongodb_connection_string).\
 config("spark.mongodb.output.uri", mongodb_connection_string).\
-config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0").\
 getOrCreate()
-df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+
+spark2 = SQLContext(spark)
+
+df = spark2.read.format("com.mongodb.spark.sql.DefaultSource").load()
 df.printSchema()
 
 
