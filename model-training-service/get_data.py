cccdeb759c02f91f06ee73049f37b70339110d75
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2021-10-17 19:02:39 +0200

    Update get_data.py

--- model-training-service/get_data.py
+++ model-training-service/get_data.py
@@ -5,33 +5,40 @@ from pyspark.streaming import StreamingContext
 from prepare_data import RemoveNonEnglishWords, Vectorize
 
 mongodb_connection_string = "mongodb+srv://IIITH-group10:LeoEXtI5sxntXmpG@cluster0.jejzt.mongodb.net/news?retryWrites=true&w=majority"
-
-client = MongoClient(mongodb_connection_string)
-db = client.news
-
 spark = SparkSession.builder.appName("newsClassifier").getOrCreate()
 spark.sparkContext.setLogLevel('WARN')
 
-newsArticles = db.newsArticles.find({}, {"_id":0, "date":0, "source":0})
-newsArticlesArr = []
-for newsArticle in newsArticles:
-	newsArticlesArr.append(newsArticle)
 
-print('Got ' + str(len(newsArticlesArr)) + ' records')
+def getData():
+	try:
+		client = MongoClient(mongodb_connection_string)
+		db = client.news
 
-df = spark.createDataFrame(newsArticlesArr)
+		newsArticles = db.newsArticles.find({}, {"_id":0, "date":0, "source":0})
+		client.close() 
 
-rddX = df.rdd.map(lambda x: str(x['title']) + ' ' + str(x['summary']))
-rddX = rddX.map(lambda x: RemoveNonEnglishWords(x))
-print('Removed non english words')
+		newsArticlesArr = []
+		for newsArticle in newsArticles:
+			newsArticlesArr.append(newsArticle)
 
-X = Vectorize(rddX.collect())
-print('Vectorized X data shape:')
-print(X.shape)
+		print('Got ' + str(len(newsArticlesArr)) + ' records')
 
-rddY = df.rdd.map(lambda x: str(x['category']))
-print('Y data shape (showing top 5):')
-print(rddY.collect()[:5])
+		df = spark.createDataFrame(newsArticlesArr)
 
+		rddX = df.rdd.map(lambda x: str(x['title']) + ' ' + str(x['summary']))
+		rddX = rddX.map(lambda x: RemoveNonEnglishWords(x))
+		print('Removed non english words')
 
+		X = Vectorize(rddX.collect())
+		print('Vectorized X data shape:')
+		print(X.shape)
+
+		Y = df.rdd.map(lambda x: str(x['category']))
+		print('Y data shape (showing top 5):')
+		print(Y.collect()[:5])
+
+		return X, Y
+	except errors.BulkWriteError as e:
+		print("Error getting data")
+       		return null     
 
