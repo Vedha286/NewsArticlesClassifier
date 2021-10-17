3f0cf19486f5ef9495bb589af82f9853e232e43c
Vedha286 <vedhakrishna.v@gmail.com>
2021-10-17 07:39:11 +0200

    py spark

--- model-training-service/get_data.py
+++ model-training-service/get_data.py
@@ -1,15 +1,34 @@
 from pymongo import MongoClient, errors
+from pyspark import SparkContext                                                                                        
+from pyspark.sql import SparkSession                                                                                    
+from pyspark.streaming import StreamingContext   
+from pyspark.sql import SQLContext
+
 
 mongodb_connection_string = "mongodb+srv://IIITH-group10:LeoEXtI5sxntXmpG@cluster0.jejzt.mongodb.net/news?retryWrites=true&w=majority"
 
 client = MongoClient(mongodb_connection_string)
 db = client.news
-newsArticles = db.newsArticles.find({})
+
+
+print('/n==================================================================/n')
+spark = SparkSession.builder.appName("newsClassifier").getOrCreate()
+print('/n==================================================================/n')
+spark.sparkContext.setLogLevel('WARN')
+print('/n==================================================================/n')
+
+newsArticles = db.newsArticles.find({}, {"_id":0})
 newsArticlesArr = []
 for newsArticle in newsArticles:
 	#print(newsArticle)
 	newsArticlesArr.append(newsArticle)
+#df = spark.createDataFrame(newsArticlesArr)
 
-print(newsArticlesArr)  # data is in array -> can convert to a dataframe    pd.Dataframe(newsArticlesArr)
+#df.show()
 
-print(len(newsArticlesArr))
+rdd = spark.sparkContext.parallelize(newsArticlesArr)
+dfFromRDD1 = rdd.toDF()
+dfFromRDD1.printSchema()
+
+
+
