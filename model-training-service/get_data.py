35156e8cdce9a9ef3188645cf58223b2b7b05d9e
Vedha286 <vedhakrishna.v@gmail.com>
2021-10-17 16:32:15 +0200

    task done

--- model-training-service/get_data.py
+++ model-training-service/get_data.py
@@ -1,38 +1,37 @@
 from pymongo import MongoClient, errors
-from pyspark import SparkContext                                                                                        
-from pyspark.sql import SparkSession                                                                                    
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext   
-from pyspark.sql import SQLContext
-
-def handle_rdd(rdd):
-	if not rdd.isEmpty():
-		print(rdd)
+from prepare_data import RemoveNonEnglishWords, Vectorize
 
 mongodb_connection_string = "mongodb+srv://IIITH-group10:LeoEXtI5sxntXmpG@cluster0.jejzt.mongodb.net/news?retryWrites=true&w=majority"
 
 client = MongoClient(mongodb_connection_string)
 db = client.news
 
-
-print('/n==================================================================/n')
 spark = SparkSession.builder.appName("newsClassifier").getOrCreate()
-print('/n==================================================================/n')
 spark.sparkContext.setLogLevel('WARN')
-print('/n==================================================================/n')
 
-newsArticles = db.newsArticles.find({}, {"_id":0})
+newsArticles = db.newsArticles.find({}, {"_id":0, "date":0, "source":0})
 newsArticlesArr = []
 for newsArticle in newsArticles:
-	#print(newsArticle)
 	newsArticlesArr.append(newsArticle)
-#df = spark.createDataFrame(newsArticlesArr)
 
-#df.show()
-print(len(newsArticlesArr))
-rdd = spark.sparkContext.parallelize(newsArticlesArr)
-dfFromRDD1 = rdd.toDF()
-dfFromRDD1.printSchema()
-rdd.foreachRDD(handle_rdd)
+print('Got data')
+df = spark.createDataFrame(newsArticlesArr)
+
+rddX = df.rdd.map(lambda x: str(x['title']) + ' ' + str(x['summary']))
+rddX = rddX.map(lambda x: RemoveNonEnglishWords(x))
+print('Removed non english words')
+
+X = Vectorize(rddX.collect())
+print('Vectorized X data:')
+print(X.shape)
+
+
+rddY = df.rdd.map(lambda x: str(x['category']))
+print('Y data:')
+print(rddY.collect())
 
 
 
