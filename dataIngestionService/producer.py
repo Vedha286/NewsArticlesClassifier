9021b94db1ff164a2955715bc18fd34afe9c44f8
Vedha286 <55736158+Vedha286@users.noreply.github.com>
2021-11-05 16:55:31 +0200

    Update producer.py

--- dataIngestionService/producer.py
+++ dataIngestionService/producer.py
@@ -121,8 +121,8 @@ def get_data_from_apis():
 	
 
 # After an interval calling the below functions
-schedule.every(24/times_per_day).seconds.do(get_data_from_apis)
-#schedule.every(24/times_per_day).hours.do(get_data_from_apis)
+#schedule.every(24/times_per_day).seconds.do(get_data_from_apis)
+schedule.every(24/times_per_day).hours.do(get_data_from_apis)
 
 while True:
     schedule.run_pending()
